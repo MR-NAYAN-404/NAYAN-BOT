b1b1ecaa5eafe08622194703558a9ff29d1bf669
MR.NAYAN <112745164+MR-NAYAN-404@users.noreply.github.com>
2023-09-19 02:37:39 +0600

    Update Nayana.js

--- Nayana.js
+++ Nayana.js
@@ -2,6 +2,7 @@ console.clear();
 const { spawn } = require("child_process");
 const express = require("express");
 const app = express();
+const axios = require("axios");
 const chalk = require('chalk');
 const logger = require("./Nayanc.js");
 const path = require('path');
@@ -10,12 +11,17 @@ app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '/website/Nayan.html'));
 });
 console.clear();
+
+
+
 function startBot(message) {
     (message) ? logger(message, "starting") : "";
+ 
   console.log(chalk.blue('⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯\n             DEPLOYING MAIN SYSTEM\n⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯\n'));
   logger.loader(`deploying app on port ${chalk.blueBright(PORT)}`);
   app.listen(logger.loader(`app deployed on port ${chalk.blueBright(PORT)}`));
-  const child = spawn("node", ["--trace-warnings", "--async-stack-traces", "Nayanb.js"], {
+  console.clear()
+  /*const child = spawn("node", ["--trace-warnings", "--async-stack-traces", "Nayanb.js"], {
         cwd: __dirname,
         stdio: "inherit",
         shell: true
@@ -26,10 +32,17 @@ function startBot(message) {
             global.countRestart += 1;
             return;
         } else return;
-    });
+   */
+axios.get("https://raw.githubusercontent.com/MR-NAYAN-404/NAYAN-BOT/main/update.json").then((res) => {
+  logger(res['data']['name'], "[ NAME ]");
+  logger("Version: " + res['data']['version'], "[ VERSION ]");
+  logger(res['data']['description'], "[ DESCRIPTION ]");
+  logger(res['data']['main'], "[ FORK ]");
+});
 
-  child.on("error", function(error) {
-    logger("an error occurred : " + JSON.stringify(error), "error");
-  });
+console.log(chalk.blue("            ┳┓┏┓┓┏┏┓┳┓  ┳┓┏┓┏┳┓\n            ┃┃┣┫┗┫┣┫┃┃━━┣┫┃┃ ┃ \n            ┛┗┛┗┗┛┛┗┛┗  ┻┛┗┛ ┻ "))
+console.log(chalk.blue(`⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯\n             NAYAN-BOT UPDATE \n⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯\n`));
+  
+  
 };
 startBot();
